98f1f365e6a79de6472bcbad0fe61d0033f126db
Shalu <88194182+shalujha123@users.noreply.github.com>
2025-03-02 10:49:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,27 +1,35 @@
-//complete this code
 class Person {
-	constructor(name,age){
-		this.name = name
-		this.age = age
-	}
-	getName(){
-		return this.name
-	}
-	setAge(age){
-		this.age = age
-	}
+    constructor(name, age) {
+        this._name = name;   // semi-private
+        this._age = age;     // semi-private
+    }
+
+    // Getter for _name
+    get name() {
+        return this._name;
+    }
+
+    // Getter for _age
+    get age() {
+        return this._age;
+    }
+
+    // Setter for age (so person.age = 30 works)
+    set age(newAge) {
+        this._age = newAge;
+    }
 }
 
 class Student extends Person {
-	study(){
-		console.log(`${this.name} is studying`)
-	}
+    study() {
+        console.log(`${this._name} is studying`);
+    }
 }
 
 class Teacher extends Person {
-	teach(){
-		console.log(`${this.name} is teaching`)
-	}
+    teach() {
+        console.log(`${this._name} is teaching`);
+    }
 }
 
 // Do not change the code below this line
@@ -29,21 +37,14 @@ window.Person = Person;
 window.Student = Student;
 window.Teacher = Teacher;
 
-// const person = new Person("John",25)
-// console.log(person.name)
+// Test example
+const person = new Person("John", 25);
+console.log(person.name);  // John
+person.age = 30;
+console.log(person.age);   // 30
 
-// person.age = 30
-// console.log(person.age)
+const student = new Student("Alice", 22);
+student.study();
 
-
-// const student = new Student("Alice",22)
-// student.study()
-
-// const teacher = new Teacher("Bob",40)
-// teacher.teach()
-
-
-
-
-
-
+const teacher = new Teacher("Bob", 40);
+teacher.teach();
